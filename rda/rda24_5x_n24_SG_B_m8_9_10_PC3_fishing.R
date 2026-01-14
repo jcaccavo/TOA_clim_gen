91dcac9282c32d3a9d72dfb13ce1104e7295b3cc
jcaccavo <goredsox747@gmail.com>
2026-01-14 10:50:35 +0100

    Update and rename rda24_5x_n24_SG_B_m8_9_10_PC3_fishing.R to rda24_5x_n24_SG_B_m8_9_10_PC3_fishing.R

---
+++ rda/rda24_5x_n24_SG_B_m8_9_10_PC3_fishing.R
@@ -0,0 +1,576 @@
+# helpful links
+# https://github.com/Tom-Jenkins/seascape_rda_tutorial/blob/master/4.Redundancy_analysis/4.redundancy_analysis.R
+# https://www.davidzeleny.net/anadat-r/doku.php/en:forward_sel_examples
+
+# run on server
+# install necessary packages using conda
+# conda install -c conda-forge r-tidyverse
+# conda install -c conda-forge r-psych
+# conda install -c conda-forge r-adespatial
+# conda install -c conda-forge r-vegan
+# conda install -c conda-forge r-rlist
+# conda install -c conda-forge r-adegenet
+
+# Run in R in directory
+
+# in R
+# Load packages
+library(tidyverse)
+library(psych)
+library(adespatial)
+library(vegan)
+library(rlist)
+library(adegenet)
+
+# Load SNPs
+snps_unlinked_5x <-read.PLINK(".../5x_TOA_only_filtered_SNPs_all_unlinked.raw")
+# Reading PLINK raw format into a genlight object... 
+# 
+# 
+# Reading loci information... 
+# 
+# Reading and converting genotypes... 
+# .
+# Building final object... 
+# 
+# ...done.
+
+dim(snps_unlinked_5x)
+# [1]      24 2359009
+
+sum(is.na(snps_unlinked_5x))
+# [1] 0
+# Warning message:
+#   In is.na(snps_unlinked_5x) :
+#   is.na() applied to non-(list or vector) of type 'S4'
+
+# Impute missing SNPs
+snps_unlinked_imp_5x <- apply(snps_unlinked_5x, 2, function(x) replace(x, is.na(x), as.numeric(names(which.max(table(x))))))
+# may take a few minutes
+
+dim(snps_unlinked_imp_5x)
+# [1]      24 2359009
+
+sum(is.na(snps_unlinked_imp_5x))
+# [1] 0
+
+#FORWARD SELECTION#########################################################################################################
+
+# Load predictor variables
+envs <- read.csv(".../rda24_5x_n24_SG_B_m8_9_10_PC3_fishing.csv")
+
+str(envs)
+# 'data.frame':   24 obs. of  4 variables:
+# $ PC1            : num  -1.32 2.44 2.26 2.26 2.26 ...
+# $ PC2            : num  -2.275 -0.739 -0.327 -0.327 -0.327 ...
+# $ PC3            : num  0.038 -0.425 -0.244 -0.244 -0.244 ...
+# $ fishing_ind_sum: int  398 0 0 0 0 0 398 398 398 356 ...
+
+#Forward selection environmental variables
+sel_fs_envs <- forward.sel(Y = snps_unlinked_imp_5x, X = envs)
+# Testing variable 1
+# Testing variable 2
+# Procedure stopped (alpha criteria): pvalue for variable 2 is 0.450000 (> 0.050000)
+
+sel_fs_envs
+# variables order         R2      R2Cum    AdjR2Cum        F pvalue
+# 1       PC1     1 0.05148626 0.05148626 0.008372001 1.194182  0.001
+
+pval.adj <- p.adjust(sel_fs_envs$pval, method = 'holm', n = 18)
+sel_fs_envs$pval.adj <- pval.adj
+sel_fs_envs
+# variables order         R2      R2Cum    AdjR2Cum        F pvalue pval.adj
+# 1       PC1     1 0.05148626 0.05148626 0.008372001 1.194182  0.001    0.018
+
+#COLLINNEARITY#########################################################################################################
+
+# check the level of correlation between predictors (should be |r| < 0.7)
+pdf("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_pair_panel_collinearity_plot_envs.pdf")
+pairs.panels(envs, scale=T)
+dev.off()
+
+# no cross-correlation between PCs or rands whatsover
+
+#CONSTRAINED RDAs#########################################################################################################
+
+#pRDA_constrained 1 all PCs (n = 3) + fishing (ind sum) as condition
+prda_1 <- rda(snps_unlinked_imp_5x ~ PC1 + PC2 + PC3 + Condition(fishing_ind_sum) , data=envs, scale=T)
+# takes <1 minute
+prda_1
+# Call: rda(formula = snps_unlinked_imp_5x ~ PC1 + PC2 + PC3 +
+#             Condition(fishing_ind_sum), data = envs, scale = T)
+# 
+# -- Model Summary --
+#   
+#   Inertia Proportion Rank
+# Total         2.359e+06  1.000e+00     
+# Conditional   1.077e+05  4.565e-02    1
+# Constrained   3.242e+05  1.374e-01    3
+# Unconstrained 1.927e+06  8.169e-01   19
+# 
+# Inertia is correlations
+# 
+# -- Eigenvalues --
+#   
+#   Eigenvalues for constrained axes:
+#   RDA1   RDA2   RDA3 
+# 129987  99249  94940 
+# 
+# Eigenvalues for unconstrained axes:
+#   PC1    PC2    PC3    PC4    PC5    PC6    PC7    PC8 
+# 152455 109451 105187 104481 103011 101979 100439 100092 
+# (Showing 8 of 19 unconstrained eigenvalues)
+
+# VIF - variance inflation factor
+vif.cca(prda_1)
+# fishing_ind_sum    PC1             PC2             PC3 
+# 1.037914        1.016953        1.003203        1.017757 
+
+# VIF all <3! will proceed 
+
+summary(eigenvals(prda_1, model = "constrained"))
+# Importance of components:
+#                         RDA1      RDA2      RDA3
+# Eigenvalue            1.30e+05 9.925e+04 9.494e+04
+# Proportion Explained  4.01e-01 3.062e-01 2.929e-01
+# Cumulative Proportion 4.01e-01 7.071e-01 1.000e+00
+
+RsquareAdj(prda_1)
+# $r.squared
+# [1] 0.1374206
+# 
+# $adj.r.squared
+# [1] 0.008815339
+
+list.save(prda_1, "rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1.rds")
+
+anova.cca(prda_1, permutations = 1000, by="terms")
+# Permutation test for rda under reduced model
+# Terms added sequentially (first to last)
+# Permutation: free
+# Number of permutations: 1000
+# 
+# Model: rda(formula = snps_unlinked_imp_5x ~ PC1 + PC2 + PC3 + Condition(fishing_ind_sum), data = envs, scale = T)
+# Df Variance      F   Pr(>F)
+# PC1       1   128254 1.2645 0.001998 **
+# PC2       1    99279 0.9788 0.596404
+# PC3       1    96644 0.9528 0.898102
+# Residual 19  1927139
+# ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+#PLOT RDAs#########################################################################################################
+
+#Set colors for plotting
+# all points by CCAMLR areas 48, 58, 88
+bg_pop1_all <- c("#00441b",
+                 "#762a83",
+                 "#762a83",
+                 "#762a83",
+                 "#762a83",
+                 "#762a83",
+                 "#00441b",
+                 "#00441b",
+                 "#00441b",
+                 "#a6dba0",
+                 "#a6dba0",
+                 "#00441b",
+                 "#a6dba0",
+                 "#00441b",
+                 "#00441b",
+                 "#762a83",
+                 "#762a83",
+                 "#a6dba0",
+                 "#00441b",
+                 "#00441b",
+                 "#00441b",
+                 "#00441b",
+                 "#00441b",
+                 "#00441b") 
+# population categories for legend
+bg_pop1 <- c("#762a83","#a6dba0","#00441b")
+
+# all points by cohort categories pre- and post-2000
+bg_cohort2_all <- c("#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#980043",
+                    "#d7b5d8",
+                    "#d7b5d8",
+                    "#d7b5d8",
+                    "#d7b5d8",
+                    "#d7b5d8",
+                    "#d7b5d8",
+                    "#d7b5d8",
+                    "#d7b5d8") 
+# population categories for legend
+bg_cohort2 <- c("#d7b5d8","#980043")
+
+# plot prda_1 by pop1 
+pdf("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1_pop1.pdf")
+plot(prda_1, type="n", scaling=3, xlab="RDA1 40.1%", ylab="RDA2 30.6%")
+points(prda_1, display="sites", pch=21, cex=1, scaling=3, bg=bg_pop1_all)
+text(prda_1, scaling=3, display="bp", col="#0868ac", cex=1)
+legend("bottomright", legend=c("48","58","88"), bty="o", col="gray32", pch=21, cex=1, pt.bg=bg_pop1)
+dev.off()
+
+# plot prda_1 by cohort2 
+pdf("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1_cohort2.pdf")
+plot(prda_1, type="n", scaling=3, xlab="RDA1 40.1%", ylab="RDA2 30.6%")
+points(prda_1, display="sites", pch=21, cex=1, scaling=3, bg=bg_cohort2_all)
+text(prda_1, scaling=3, display="bp", col="#0868ac", cex=1)
+legend("bottomright", legend=c("pre2000","post2000"), bty="o", col="gray32", pch=21, cex=1, pt.bg=bg_cohort2)
+dev.off()
+
+# screeplot of prda_1
+pdf("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1_screeplot.pdf")
+screeplot(prda_1)
+dev.off()
+
+# CANDIDATE SNPS#########################################################################################################
+
+# useful links
+# https://bookdown.org/hhwagner1/LandGenCourse_book/WE_11.html
+# https://popgen.nescent.org/2018-03-27_RDA_GEA.html#conclusions
+
+# Load predictor variables
+envs <- read.csv(".../rda24_5x_n24_SG_B_m8_9_10_PC3_fishing.csv")
+
+str(envs)
+# 'data.frame':   24 obs. of  4 variables:
+# $ PC1            : num  -1.32 2.44 2.26 2.26 2.26 ...
+# $ PC2            : num  -2.275 -0.739 -0.327 -0.327 -0.327 ...
+# $ PC3            : num  0.038 -0.425 -0.244 -0.244 -0.244 ...
+# $ fishing_ind_sum: int  398 0 0 0 0 0 398 398 398 356 ...
+
+# subset predictor variables to only include variables identified as significant from the ANOVA, as well as fake variables
+envs_retained <- subset(envs, select=c(PC1))
+
+str(envs_retained)
+# 'data.frame':   24 obs. of  1 variable:
+# $ PC1: num  -1.32 2.44 2.26 2.26 2.26 ...
+
+# if continuing directly from RDAs, skip loading the RDA and SNPs
+
+# ***********************************************************************************************************************
+
+# if coming back to an old analysis in a new R workspace, you must load the RDA, as well as the SNPs, and impute them
+
+# Load selected RDA
+prda_1 <- read_rds("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1.rds")
+
+# Load SNPs
+snps_unlinked_5x <-read.PLINK(".../5x_TOA_only_filtered_SNPs_all_unlinked.raw")
+# Reading PLINK raw format into a genlight object... 
+# 
+# 
+# Reading loci information... 
+# 
+# Reading and converting genotypes... 
+# .
+# Building final object... 
+# 
+# ...done.
+
+# Impute missing SNPs
+snps_unlinked_imp_5x <- apply(snps_unlinked_5x, 2, function(x) replace(x, is.na(x), as.numeric(names(which.max(table(x))))))
+# may take a few minutes
+length(snps_unlinked_imp_5x)
+# [1] 56,616,216
+
+# ***********************************************************************************************************************
+
+# Extract SNP loadings for all axes (RDA 1 - 3, PC 1- 19)
+load.rda <- scores(prda_1, choices=c(1:22), display="species")
+
+# Plot histograms of SNP loadings from RDA axes
+pdf("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1_load_constrained_1.pdf")
+hist(load.rda[,1], main="Loadings on RDA1")
+hist(load.rda[,2], main="Loadings on RDA2")
+hist(load.rda[,3], main="Loadings on RDA3")
+dev.off()
+
+# Identify SNPs in the tails of the distribution
+outliers <- function(x,z){
+  lims <- mean(x) + c(-1, 1) * z * sd(x)     # find loadings +/-z sd from mean loading     
+  x[x < lims[1] | x > lims[2]]               # locus names in these tails
+}
+
+# Candidate SNPs per axis using z = 3 standard deviations as a cut off
+
+# RDA 1
+cand1 <- outliers(load.rda[,1],3)
+length(cand1)
+# [1] 1010
+
+# RDA 2
+cand2 <- outliers(load.rda[,2],3)
+length(cand2)
+# [1] 2254
+
+# RDA 3
+cand3 <- outliers(load.rda[,3],3)
+length(cand3)
+# [1] 14664
+
+# PC 1
+cand4 <- outliers(load.rda[,4],3)
+length(cand4)
+# [1] 21
+
+# PC 2
+cand5 <- outliers(load.rda[,5],3)
+length(cand5)
+# [1] 1811
+
+# PC 3
+cand6 <- outliers(load.rda[,6],3)
+length(cand6)
+# [1] 2315
+
+# PC 4
+cand7 <- outliers(load.rda[,7],3)
+length(cand7)
+# [1] 927
+
+# PC 5
+cand8 <- outliers(load.rda[,8],3)
+length(cand8)
+# [1] 2587
+
+# PC 6
+cand9 <- outliers(load.rda[,9],3)
+length(cand9)
+# [1] 2966
+
+# PC 7
+cand10 <- outliers(load.rda[,10],3)
+length(cand10)
+# [1] 14229
+
+# PC 8
+cand11 <- outliers(load.rda[,11],3)
+length(cand11)
+# [1] 5210
+
+# PC 9
+cand12 <- outliers(load.rda[,12],3)
+length(cand12)
+# [1] 3285
+
+# PC 10
+cand13 <- outliers(load.rda[,13],3)
+length(cand13)
+# [1] 6348
+
+# PC 11
+cand14 <- outliers(load.rda[,14],3)
+length(cand14)
+# [1] 2091
+
+# PC 12
+cand15 <- outliers(load.rda[,15],3)
+length(cand15)
+# [1] 11733
+
+# PC 13
+cand16 <- outliers(load.rda[,16],3)
+length(cand16)
+# [1] 9315
+
+# PC 14
+cand17 <- outliers(load.rda[,17],3)
+length(cand17)
+# [1] 2482
+
+# PC 15
+cand18 <- outliers(load.rda[,18],3)
+length(cand18)
+# [1] 5348
+
+# PC 16
+cand19 <- outliers(load.rda[,19],3)
+length(cand19)
+# [1] 8523
+
+# PC 17
+cand20 <- outliers(load.rda[,20],3)
+length(cand20)
+# [1] 10412
+
+# PC 18
+cand21 <- outliers(load.rda[,21],3)
+length(cand21)
+# [1] 3225
+
+# PC 19
+cand22 <- outliers(load.rda[,22],3)
+length(cand22)
+# [1] 3671
+
+# all output values (for future comparison)
+# [1] 1010
+# [1] 2254
+# [1] 14664
+# [1] 21
+# [1] 1811
+# [1] 2315
+# [1] 927
+# [1] 2587
+# [1] 2966
+# [1] 14229
+# [1] 5210
+# [1] 3285
+# [1] 6348
+# [1] 2091
+# [1] 11733
+# [1] 9315
+# [1] 2482
+# [1] 5348
+# [1] 8523
+# [1] 10412
+# [1] 3225
+# [1] 3671
+
+# Candidate total from both RDA (3) and PC (20) axes
+ncand_all <- length(cand1) + length(cand2) + length(cand3) + length(cand4) + length(cand5) + length(cand6) + length(cand7) + length(cand8) + length(cand9) + length(cand10) + length(cand11) + length(cand12) + length(cand13) + length(cand14) + length(cand15) + length(cand16) + length(cand17) + length(cand18) + length(cand19) + length(cand20) + length(cand21) + length(cand22)
+ncand_all
+# [1] 114427
+
+# Candidate total from RDA axes only
+ncand_rda3 <- length(cand1) + length(cand2) + length(cand3)
+ncand_rda3
+# [1] 17928
+
+# Candidate total from RDA axes 1 and 2 only
+ncand_rda2 <- length(cand1) + length(cand2)
+ncand_rda2
+# [1] 3264
+
+# Candidate total from RDA axis 1 only
+ncand_rda1 <- length(cand1)
+ncand_rda1
+# [1] 1010
+
+# Candidate SNP names from RDA axes 1 and 2
+prda1cand2 <- c(names(cand1), names(cand2))
+
+# Candidate SNP names from RDA axes 1 and 2
+prda1cand3 <- c(names(cand1), names(cand2), names(cand3))
+
+# Candidate SNPs detected on multiple RDA axes
+length(prda1cand3[duplicated(prda1cand3)])
+# [1] 0
+# No duplicates
+
+# Candidate SNPs detected on RDA axes 1 and 2
+length(prda1cand2[duplicated(prda1cand2)])
+# [1] 0
+# No duplicates
+
+# In the event that there are duplicates, use the following to remove
+# prda1cand3 <- prda1cand3[!duplicated(prda1cand3)] 
+# prda1cand2 <- prda1cand2[!duplicated(prda1cand2)] 
+
+# Set up the color scheme for plotting:
+bgcol  <- ifelse(colnames(snps_unlinked_imp_5x) %in% prda1cand2, 'gray32', '#00000000')
+snpcol <- ifelse(colnames(snps_unlinked_imp_5x) %in% prda1cand2, 'red', '#00000000')
+
+## axes 1 & 2 - zooming in to just the SNPs here...
+pdf("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1_SNP_zoom_axes1_2.pdf")
+plot(prda_1, type="n", scaling=3, xlim=c(-1,1), ylim=c(-1,1), main="rda24.prda1, axes 1 and 2", xlab="RDA1 40.1%", ylab="RDA2 30.6%")
+points(prda_1, display="species", pch=21, cex=1, col="gray32", bg='#f1eef6', scaling=3)
+points(prda_1, display="species", pch=21, cex=1, col=bgcol, bg=snpcol, scaling=3)
+text(prda_1, scaling=3, display="bp", col="#0868ac", cex=1)
+dev.off()
+
+pdf("rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1_SNP_zoom_axes2_3.pdf")
+plot(prda_1, type="n", scaling=3, xlim=c(-1,1), ylim=c(-1,1), choices=c(2,3), main="rda24.prda1, axes 2 and 3", xlab="RDA1 40.1%", ylab="RDA2 30.6%")
+points(prda_1, display="species", pch=21, cex=1, col="gray32", bg='#f1eef6', scaling=3)
+points(prda_1, display="species", pch=21, cex=1, col=bgcol, bg=snpcol, scaling=3)
+text(prda_1, scaling=3, display="bp", col="#0868ac", cex=1)
+dev.off()
+
+# Let’s see which environmental predictors are most strongly correlated with the first three RDA axes
+intersetcor(prda_1)[,1:3]
+#         RDA1       RDA2        RDA3
+# PC1  0.93832263 0.04197081 -0.21886628
+# PC2 -0.03965053 0.99761377  0.04084409
+# PC3 -0.22766396 0.02261746 -0.97124622
+
+# candidate SNPs on RDA1 represent multilocus sets of SNP genotypes associated most strongly with PC1
+# candidate SNPs on RDA2 represent multilocus sets of SNP genotypes associated most strongly with PC2
+# candidate SNPs on RDA3 represent multilocus sets of SNP genotypes associated most strongly with PC3
+
+# organize results into one data frame with the axis, SNP name, loading, & correlation with each predictor
+cand1 <- cbind.data.frame(rep(1,times=length(cand1)), names(cand1), unname(cand1))
+cand2 <- cbind.data.frame(rep(2,times=length(cand2)), names(cand2), unname(cand2))
+cand3 <- cbind.data.frame(rep(3,times=length(cand3)), names(cand3), unname(cand3))
+colnames(cand1) <- colnames(cand2) <- colnames(cand3) <- c("axis","snp","loading")
+cand <- rbind(cand1, cand2, cand3)
+cand$snp <- as.character(cand$snp)
+
+# add in the correlations of each candidate SNP with predictor (n = 1) PC1
+foo <- matrix(nrow=(ncand_rda3), ncol=1)  # 1 column for 1 predictor
+colnames(foo) <- c("PC1")
+
+for (i in 1:length(cand$snp)) {
+  nam <- cand[i,2]
+  snp.gen <- snps_unlinked_imp_5x[,nam]
+  foo[i,] <- apply(envs_retained,2,function(x) cor(x,snp.gen))
+}
+# may take a few minutes
+
+cand <- cbind.data.frame(cand,foo)  
+head(cand)
+#     axis                   snp     loading        PC1
+# 1    1  HiC_scaffold_10:564698_G  0.04553554  0.7923422
+# 2    1 HiC_scaffold_10:1503610_G  0.04172289  0.6737594
+# 3    1 HiC_scaffold_10:2543556_T  0.04413212  0.7915214
+# 4    1 HiC_scaffold_10:2711464_A  0.04205947  0.7606572
+# 5    1 HiC_scaffold_10:3231239_G -0.03366827 -0.6494701
+# 6    1 HiC_scaffold_10:3720955_G -0.03402031 -0.5867432
+
+length(cand$snp[duplicated(cand$snp)])
+# takes <1 minute
+# [1] 0
+
+foo <- cbind(cand$axis, duplicated(cand$snp))
+table(foo[foo[,1]==1,2]) # no duplicates on axis 1
+# 0 
+# 1010
+table(foo[foo[,1]==2,2]) # no duplicates on axis 2
+# 0 
+# 2254
+table(foo[foo[,1]==3,2]) # no duplicates on axis 3
+# 0 
+# 14664
+
+# to which predictor is each candidate SNP most strongly correlated 
+for (i in 1:length(cand$snp)) {
+  bar <- cand[i,]
+  cand[i,5] <- names(which.max(abs(bar[4:4]))) # gives the variable | column 4 starts the variables, so 4:n where n depends on how many variables you have | and i,k where k is the new column you will add, so k = n + 1
+  cand[i,6] <- max(abs(bar[4:4]))              # gives the correlation | in the brackets [] include the same columns indicated in the line above in brackets | and i,m where m is the second new column you will add, so m = k + 1
+}
+colnames(cand)[5] <- "predictor"
+colnames(cand)[6] <- "correlation"
+
+table(cand$predictor)
+# PC1 
+# 17928 
+
+# this is the total candidate SNPs explained all RDA axes
+
+write.csv(cand,"rda24_5x_n24_SG_B_m8_9_10_PC3_fishing_prda_1_putative_adaptive_SNPs.csv")
